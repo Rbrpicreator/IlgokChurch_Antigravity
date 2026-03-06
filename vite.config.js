deeac1530adf7be6c79b7a9a313565308f2ae292
Ji Hun Yoon <97179959+Rbrpicreator@users.noreply.github.com>
2026-03-06 23:26:17 +0900

    Set base path for Vite configuration
    Add base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 반드시 양옆에 슬래시(/)가 있어야 합니다!
+  base: '/IlgokChurch_Antigravity/', 
 })
